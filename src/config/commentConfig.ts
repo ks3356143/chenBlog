11c26b2945656b929f8766deb77f3c958cf32922
chenjunyi <314298729@qq.com>
2026-04-15 18:03:15 +0800

    v0.1.1

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -1,10 +1,12 @@
+const isDev = import.meta.env.DEV
+
 export const commentConfig = {
     type: "twikoo",
     twikoo: {
-        envId: "http://47.108.230.220:8099/",
+        envId: isDev ? "http://47.108.230.220:8099/" : "http://localhost:8099",
         // 设置 Twikoo 评论系统语言
         lang: "zh-CN",
         // 是否启用文章访问量统计功能
-        visitorCount: true,
+        visitorCount: false,
     },
 }
